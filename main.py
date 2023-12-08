5a9dac12f64541a7a48fd082f43954383ef6fa85
Miguel Olave <molavec@gmail.com>
2023-12-08 14:27:12 -0300

    ajusta tabs size

--- main.py
+++ main.py
@@ -8,22 +8,22 @@ app = FastAPI()
 
 @app.get("/", response_class=HTMLResponse)
 async def root():
-    return """
+  return """
     <html>
-        <head>
-            <title>Test API</title>
+      <head>
+        <title>Test API</title>
         </head>
         <body>
             <h1>Hello Test API</h1>
         </body>
     </html>
-    """
+"""
 
 
 @app.get("/programs/", response_class=ORJSONResponse)
 async def programs():
-    return ORJSONResponse(programsData.programs)
+  return ORJSONResponse(programsData.programs)
 
 @app.get("/programs/{itemId}", response_class=ORJSONResponse)
 async def programs(itemId: int):
-    return ORJSONResponse([d for d in programsData.programs if d['id'] in [itemId]])
+  return ORJSONResponse([d for d in programsData.programs if d['id'] in [itemId]])
